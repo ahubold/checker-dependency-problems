1ba79f4d9fb999d53e41b5752e13e7d2faa3e6a3
Andreas Hubold <andreas.hubold@coremedia.com>
2017-12-21 17:01:55 +0100

    add minimal example project

---
+++ custom-checker/src/main/java/com/example/checker/CustomChecker.java
@@ -0,0 +1,6 @@
+package com.example.checker;
+
+import org.checkerframework.common.basetype.BaseTypeChecker;
+
+public class CustomChecker extends BaseTypeChecker {
+}
---
+++ other/src/main/java/com/example/other/Other.java
@@ -0,0 +1,11 @@
+package com.example.other;
+
+import com.google.common.collect.ImmutableList;
+
+public class Other {
+
+  public void m() {
+    ImmutableList.of(); // Guava usage
+  }
+
+}
